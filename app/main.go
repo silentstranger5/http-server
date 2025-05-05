c51778bcac9eef4e35d33a20d74f75adf9fc9bc9
Nikita Smirnov <n.smirnov1572@gmail.com>
2025-05-05 17:56:06 +0300

    respond with body

--- app/main.go
+++ app/main.go
@@ -24,51 +24,101 @@ func main() {
 		os.Exit(1)
 	}
 
-	handleConn(conn)
+	handle_conn(conn)
 }
 
-func handleConn(conn net.Conn) {
+func handle_conn(conn net.Conn) {
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("Failed to read connection:", err)
 		os.Exit(1)
 	}
-	request := strings.Split(string(buf), "\r\n")
-	reqline := request[0]
-	// reqheaders := request[1 : len(request)-2]
-	// reqbody := request[len(request)-1]
+
+	req := parse_request(buf)
+	res := handle_request(req)
+	buf = serialize_response(res)
+
+	_, err = conn.Write(buf)
+	if err != nil {
+		fmt.Println("Failed to write to connection:", err)
+		os.Exit(1)
+	}
+	conn.Close()
+}
+
+type request struct {
+	method  string
+	target  string
+	version string
+	headers []string
+	body    string
+}
+
+func parse_request(buf []byte) request {
+	req := strings.Split(string(buf), "\r\n")
+	reqline := req[0]
+	reqheaders := req[1 : len(req)-2]
+	reqbody := req[len(req)-1]
 
 	reqparts := strings.Split(reqline, " ")
+	reqmethod := reqparts[0]
 	reqtarget := reqparts[1]
-	// reqmethod := reqparts[0]
-	// reqversion := reqparts[2]
+	reqversion := reqparts[2]
 
+	return request{
+		method:  reqmethod,
+		target:  reqtarget,
+		version: reqversion,
+		headers: reqheaders,
+		body:    reqbody,
+	}
+}
+
+type response struct {
+	version string
+	code    string
+	phrase  string
+	body    string
+	headers []string
+}
+
+func handle_request(req request) response {
 	resversion := "HTTP/1.1"
-	resstatuscode := "200"
-	resstatusphrase := "OK"
+	rescode := "200"
+	resphrase := "OK"
+	resheaders := make([]string, 0)
+	resbody := ""
 
-	if reqtarget != "/" {
-		resstatuscode = "400"
-		resstatusphrase = "Not Found"
+	if req.target == "/" {
+		return response{resversion, rescode, resphrase, resbody, resheaders}
 	}
 
-	resstatus := strings.Join(
-		[]string{
-			resversion,
-			resstatuscode,
-			resstatusphrase,
-		},
-		" ")
-	resheaders := ""
-	resbody := ""
-	response := strings.Join(
-		[]string{
-			resstatus,
-			resheaders,
-			resbody,
-		},
-		"\r\n")
-	conn.Write([]byte(response))
-	conn.Close()
+	targetparts := strings.Split(req.target, "/")
+	if len(targetparts) < 3 {
+		rescode = "400"
+		resphrase = "Not Found"
+		return response{resversion, rescode, resphrase, resbody, resheaders}
+	}
+
+	method := targetparts[1]
+	switch method {
+	case "echo":
+		msg := targetparts[2]
+		resheaders = append(resheaders, "Content-Type: text/plain")
+		resheaders = append(resheaders, fmt.Sprintf("Content-Length: %d", len(msg)))
+		resheaders = append(resheaders, "")
+		resbody = msg
+	default:
+		rescode = "400"
+		resphrase = "Not Found"
+	}
+
+	return response{resversion, rescode, resphrase, resbody, resheaders}
+}
+
+func serialize_response(res response) []byte {
+	statusline := strings.Join([]string{res.version, res.code, res.phrase}, " ")
+	headers := strings.Join(res.headers, "\r\n")
+	return []byte(strings.Join([]string{statusline, headers, res.body}, "\r\n"))
 }
