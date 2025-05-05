a7d1dabbdaa732de0bf032f691aa7c6d6c86435d
Nikita Smirnov <n.smirnov1572@gmail.com>
2025-05-05 18:08:45 +0300

    concurrent connections

--- app/main.go
+++ app/main.go
@@ -18,13 +18,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	handle_conn(conn)
+		go handle_conn(conn)
+	}
 }
 
 func handle_conn(conn net.Conn) {
