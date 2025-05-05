549296961eb8b286f937e0ab56b6b4e959aea2df
Nikita Smirnov <n.smirnov1572@gmail.com>
2025-05-05 16:08:27 +0300

    respond with 200

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
@@ -17,9 +18,41 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handleConn(conn)
+}
+
+func handleConn(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Failed to read connection")
+		os.Exit(1)
+	}
+	version := "HTTP/1.1"
+	statuscode := "200"
+	statusphrase := "OK"
+	status := strings.Join(
+		[]string{
+			version,
+			statuscode,
+			statusphrase,
+		},
+		" ")
+	headers := ""
+	body := ""
+	msg := strings.Join(
+		[]string{
+			status,
+			headers,
+			body,
+		},
+		"\r\n")
+	conn.Write([]byte(msg))
+	conn.Close()
 }
