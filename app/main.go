41564f7bb262776e58bace64c7118176b5cbc7ad
Nikita Smirnov <n.smirnov1572@gmail.com>
2025-05-05 16:55:06 +0300

    extract url path

--- app/main.go
+++ app/main.go
@@ -31,28 +31,44 @@ func handleConn(conn net.Conn) {
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Failed to read connection")
+		fmt.Println("Failed to read connection:", err)
 		os.Exit(1)
 	}
-	version := "HTTP/1.1"
-	statuscode := "200"
-	statusphrase := "OK"
-	status := strings.Join(
+	request := strings.Split(string(buf), "\r\n")
+	reqline := request[0]
+	// reqheaders := request[1 : len(request)-2]
+	// reqbody := request[len(request)-1]
+
+	reqparts := strings.Split(reqline, " ")
+	reqtarget := reqparts[1]
+	// reqmethod := reqparts[0]
+	// reqversion := reqparts[2]
+
+	resversion := "HTTP/1.1"
+	resstatuscode := "200"
+	resstatusphrase := "OK"
+
+	if reqtarget != "/" {
+		resstatuscode = "400"
+		resstatusphrase = "Not Found"
+	}
+
+	resstatus := strings.Join(
 		[]string{
-			version,
-			statuscode,
-			statusphrase,
+			resversion,
+			resstatuscode,
+			resstatusphrase,
 		},
 		" ")
-	headers := ""
-	body := ""
-	msg := strings.Join(
+	resheaders := ""
+	resbody := ""
+	response := strings.Join(
 		[]string{
-			status,
-			headers,
-			body,
+			resstatus,
+			resheaders,
+			resbody,
 		},
 		"\r\n")
-	conn.Write([]byte(msg))
+	conn.Write([]byte(response))
 	conn.Close()
 }
